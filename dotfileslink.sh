6f04c0c6a3aa772a38aab1b0158527c0f6e78af7
AKANISHI Marron <marron.general@gmail.com>
2025-11-08 23:47:41 +0900

    Arch Linux Settings

--- dotfileslink.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -s ~/dotfiles/.zshrc ~/.zshrc
